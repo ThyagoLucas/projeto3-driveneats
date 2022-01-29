19cb22035c44b420fa98bef973c30bf7d9699828
ThyagoLucas <=>
2022-01-29 02:12:10 -0400

    Implementação do JS na opção de comida e bebida, restando somente a opção de sobremesa para implementar. Nesso commit tbm estão armazenado os dados do pedido em variaveis.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,53 @@
-function alterar(){
-    const opcao1 = document.querySelector(".prato");
-	opcao1.classList.toggle("selecionado");    
+
+let tipoPrato =  null;
+let precoPrato = null;
+let tipoBebida = null;
+let precoBebida = null;
+let tipoSobremesa = null;
+let precoSobremesa = null;
+
+
+function variaveispedido(){
+
 }
+
+function selecionarPrato(pratoX, tipoPratoX, precoPratoX){
+
+    const selecionaP = document.querySelector(".selecionado");
+
+    if(selecionaP != null){
+        
+        selecionaP.classList.remove("selecionado");
+    }
+
+
+    //adicionar
+    const adicionaP = document.querySelector("."+pratoX);
+    adicionaP.classList.add("selecionado");
+
+    tipoPrato = tipoPratoX;
+    precoPrato = precoPratoX;
+   
+}
+
+
+
+function selecionarBebida(bebidaX, tipoBebidaX, precoBebidaX ){
+
+    const selecionaB = document.querySelector(".opcoes-bebida .selecionado");
+
+    if(selecionaB != null){
+        
+        selecionaB.classList.remove("selecionado");
+    }
+
+    //adicionar
+    const adicionaB = document.querySelector("."+bebidaX);
+    adicionaB.classList.add("selecionado");
+
+    tipoBebida = tipoBebidaX;
+    precoBebida = precoBebidaX;
+    
+}
+
 
